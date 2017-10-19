ac0318971347314caa657d7081415c551347dc07
reno- <contact@rubiano.fr>
2017-10-19 18:03:23 +0200

    update

--- examples/my_remoteUI.py
+++ examples/my_remoteUI.py
@@ -3,21 +3,45 @@
 
 """
 This file is an example of a device
-with I/O communication provided by libossia
+with I/O communication provided by libossia with pyqt5 GUI
 """
 
-import pyossia
-from pyossia import ossia, add_param
-from pxst_widgets.panel import Panel
 import sys
-from PyQt5.Qt import QThread
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QGridLayout, QGroupBox, QLabel, QListWidgetItem
+import pxst_widgets
+from pyossia import ossia
+from zeroconf import ServiceBrowser, Zeroconf
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.Qt import pyqtSignal, QObject, QThread
+from pxst_widgets.device_view import DeviceView
+
+
+class ZeroConfListener(QThread):
+    add_device = pyqtSignal(str, str, int)
+    remove_device = pyqtSignal(str)
+    def __init__(self, *args, **kwargs):
+        super(ZeroConfListener, self).__init__()
+        self.__devices__ = {}
+        zeroconf = Zeroconf()
+        browser = ServiceBrowser(zeroconf, "_oscjson._tcp.local.", self)
+
+    def remove_service(self, zeroconf, type, name):
+        name = name.split('.' + type)[0]
+        self.__devices__.pop(name)
+        self.remove_device.emit(name)
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        name = name.split('.' + type)[0]
+        port = info.port
+        server = info.server
+        self.__devices__.setdefault(name, {'server': server, 'port':port})
+        self.add_device.emit(name, server, port)
 
 
 # create the UI now
 # this could be another app that control the Pyossia Test Device
 
-class MainWindow(QMainWindow, QThread):
+class MainWindow(QMainWindow):
     """
     Main Window Doc String
     """
@@ -27,33 +51,35 @@ class MainWindow(QMainWindow, QThread):
         qss = open("style.qss", "r").read()
         self.setStyleSheet(qss)
         self.setAutoFillBackground(True)
-        # create a layout for widgets
-        self.gui = QGroupBox('osc query explorer')
-        self.layout().addWidget(self.gui)
-        self.gui_layout = QGridLayout()
-        self.gui.setLayout(self.gui_layout)
-        devices = QListWidget()
-        try:
-            device = ossia.OSCQueryDevice('my_device', 'ws://127.0.0.1:3456', 5678)
-            devices.addItem('test device')
-            device.update()
-        except RuntimeError:
-            print('no devices found on the network')
-        panel = Panel('test device remote')
-        if len(devices) > 0:
-            devices.setCurrentRow(0)
-            for node in device.get_parameters():
-                panel.add_remote(node.parameter)
-        self.gui_layout.addWidget(QLabel('devices found on the network'), 0, 0)
-        self.gui_layout.addWidget(devices, 1, 0)
-        self.gui_layout.addWidget(QLabel('device remote'), 0, 1)
-        self.gui_layout.addWidget(panel, 1, 1)
-        devices.currentItemChanged.connect(self.selection_changed)
-        self.move(0, 40)
-        self.setMinimumSize(self.gui.size())
+        #self.move(0, 40)
+        # start the callback, it will create items
+        listener = ZeroConfListener()
+        listener.add_device.connect(self.add_device)
+        listener.remove_device.connect(self.remove_device)
 
-    def selection_changed(self, truc):
-        print('GO ---', truc.text(), type(truc))
+    def add_device(self, name, server, port):
+        print('yeah')
+        try:
+            target = 'ws://' + server + ':' + str(port)
+            mirror_device = ossia.OSCQueryDevice("Explorer for " + name, target, 5678)
+            print('OK, device ready')
+        except RuntimeError:
+            print('Exception raised. Is it the best way?')
+            target = 'http://' + server + ':' + str(port)
+            mirror_device = ossia.OSCQueryDevice("Explorer for " + name, target, 5678)
+                # Grab the namespace with an update
+        print('update it now')
+        mirror_device.update()
+        description = name + ' on ' + server + ':' + str(port)
+        print('ADDED ' + str(name))
+        # Draw an UI for local_device
+        self.panel = DeviceView(device=mirror_device, width='auto', height='auto')
+        # assign this device to the mainwindow
+        self.setCentralWidget(self.panel)
+        self.setFixedSize(self.centralWidget().width(), self.centralWidget().height())
+
+    def remove_device(self, name):
+        print('BYE BYE ' + name)
 
 
 if __name__ == "__main__":
